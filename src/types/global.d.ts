db858e2521abd3b49870cae08a5f41f7959def81
demd7362 <demd7362@gmail.com>
2023-11-04 10:12:26 +0900

    refactor : Response 코드 리팩토링

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -38,33 +38,42 @@ type DateTime = {
     date?: Date;
 };
 
-interface Response {
+type Response = {
     status: string;
     message: string;
 }
 
 type Pagination = {
-    page: number,
-    pageSize: number,
-    offset?: number,
-    totalPage: number,
+    page: number;
+    pageSize: number;
+    offset?: number;
+    totalPage: number;
 }
 
 type QuizInfo = {
-    qiNum?: number,
-    qiId: string,
-    qiTitle: string,
-    qiContent: string,
-    qiAnswer: number,
-    qiLevel: number,
-    qiActive: number,
-    qiCreated?: string,
-    qiChanged?: string
+    qiNum?: number;
+    qiId: string;
+    qiTitle: string;
+    qiContent: string;
+    qiAnswer: number;
+    qiLevel: number;
+    qiActive: number;
+    qiCreated?: string;
+    qiChanged?: string;
 }
 
 type QuizSubmit = {
-    qsNum?:number,
-    qsId:string,
-    qsTry?:number,
-    qsSolved?:number
+    qsNum?:number;
+    qsId:string;
+    qsTry?:number;
+    qsSolved?:number;
+}
+type PaginationProps = {
+    pagination: Pagination;
+    handlePrev: () => void;
+    handleNext: () => void;
+    handleClickPage: (page: number) => void;
+    prevText: string;
+    nextText: string;
+    bottomSize: number;
 }
